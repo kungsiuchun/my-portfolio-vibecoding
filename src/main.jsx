f0c4684b03a5785728801b3e23217c45e92edc5c
kungsiuchun <34229709+kungsiuchun@users.noreply.github.com>
2025-12-26 18:20:48 +0000

    在 main.jsx 加入補丁

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,15 @@
+// 為舊版瀏覽器 (如 react-snap 內建的 Chrome) 手動加入 Object.hasOwn 的補丁
+if (!Object.hasOwn) {
+  Object.defineProperty(Object, "hasOwn", {
+    value: function (instance, prop) {
+      return Object.prototype.hasOwnProperty.call(instance, prop);
+    },
+    configurable: true,
+    enumerable: false,
+    writable: true,
+  });
+}
+
 import React from 'react';
 import { hydrateRoot, createRoot } from 'react-dom/client';
 import App from './App';
