1cd4c1cd13551f585b3f01bba790fbd9a00b69d3
kungsiuchun <34229709+kungsiuchun@users.noreply.github.com>
2025-12-26 11:01:33 -0800

    back to toast stage

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,10 @@
-// 為舊版瀏覽器 (如 react-snap 內建的 Chrome) 手動加入 Object.hasOwn 的補丁
-if (!Object.hasOwn) {
-  Object.defineProperty(Object, "hasOwn", {
-    value: function (instance, prop) {
-      return Object.prototype.hasOwnProperty.call(instance, prop);
-    },
-    configurable: true,
-    enumerable: false,
-    writable: true,
-  });
-}
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-import React from 'react';
-import { hydrateRoot, createRoot } from 'react-dom/client';
-import App from './App';
-import './index.css';
-
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
